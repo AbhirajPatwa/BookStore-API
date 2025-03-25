76aaa11e64fa664b172aeb54b9a857b0f2c4935f
Abhiraj Patwa <abhirajpatwa930@gmail.com>
2025-03-25 15:59:18 +0000

    init

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"github.com/AbhirajPatwa/url-shortener/config"
+	"github.com/AbhirajPatwa/url-shortener/controller"
+	
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"hello": "world",
+		})
+	})
+	bookRoute := e.Group("/book")
+	bookRoute.POST("/", controller.CreateBook)
+	bookRoute.GET("/:id", controller.GetBook)
+	bookRoute.PUT("/:id", controller.UpdateBook)
+	bookRoute.DELETE("/:id", controller.DeleteBook)
+	e.Logger.Fatal(e.Start(":8080"))
+
+	// Connect To Database
+	config.DatabaseInit()
+	gorm := config.DB()
+
+	dbGorm, err := gorm.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	dbGorm.Ping()
+	
+	e.Logger.Fatal(e.Start(":8080"))
+}
